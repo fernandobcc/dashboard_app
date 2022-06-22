c0fbb6e7b41b699791b3e4ecbf485203379edf56
fernandobcc <fernandoxico@gmail.com>
2022-06-22 19:50:35 -0300

    feat: setup frontend project

---
+++ Frontend/src/pages/SignIn/index.tsx
@@ -0,0 +1,9 @@
+const SignIn: React.FC = () => {
+  return (
+    <div>
+      <h1>SignIn</h1>
+    </div>
+  )
+}
+
+export default SignIn
---
+++ frontend/src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import Dashboard from './pages/Dashboard'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+  <React.StrictMode>
+    <Dashboard />
+  </React.StrictMode>
+)
