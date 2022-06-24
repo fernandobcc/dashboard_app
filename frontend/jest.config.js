ea9ed8d751f862bff19739a46393c779d9323032
fernandobcc <fernandoxico@gmail.com>
2022-06-24 00:18:02 -0300

    feat: add template generator for components

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,7 +2,19 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/index.tsx',
+    '!src/App.tsx',
+    '!src/styles/**/*.ts',
+    '!src/**/stories.tsx',
+    '!src/pages/**/*.tsx',
+    '!src/utils/apollo.ts',
+    '!src/utils/apolloCache.ts',
+    '!src/types/**/*.d.ts',
+    '!src/graphql/**/*.ts',
+    '!src/**/mock.ts'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
   transform: {
